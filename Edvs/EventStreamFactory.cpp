89206eb2dcf84f3ed78c09df98b77ee0afe85114
David Weikersdorfer <davidw@danvil.de>
2013-04-11 20:32:39 +0200

    Basic URI parsing to decide stream type

--- Edvs/EventStreamFactory.cpp
+++ Edvs/EventStreamFactory.cpp
@@ -36,49 +36,48 @@ namespace Edvs
 
 	EventStreamHandle OpenURI(const std::string& uri)
 	{
-		std::cout << "Network: " << uri << std::endl;
-		// // check uri
-		// // find :
-		// bool has_colon = uri.find(':') != uri.end();
-		// bool has_point = uri.find('.') != uri.end();
-		// bool has_question = uri.find('?') != uri.end();
-		// bool has_equal = uri.find('=') != uri.end();
-		// bool has_slash = uri.find('/') != uri.end();
-		// // guess
-		// int[3] order = {0, 1, 2};
-		// if(has_colon) {
-		// 	order
-		// }
+		// check uri
+		bool has_colon = (uri.find(':') != std::string::npos);
+		bool has_question = (uri.find('?') != std::string::npos);
+		bool has_equal = (uri.find('=') != std::string::npos);
 		// try network
-		try {
-			DeviceHandle dh = OpenNetworkDevice(uri);
-			return EventStreamHandle(new DeviceEventStream(dh));
-		}
-		catch(const NetSocket::Exception& e) {
-			std::cerr << e.what() << std::endl;
-		};
-		// try serial
-		try {
-			std::vector<std::string> v;
-			boost::split(v, uri, boost::is_any_of("?="));
-			Baudrate br = B4000k;
-			if(v.size() == 3 && v[1] == "baudrate") {
-				if(v[2] == "921k") br = B921k;
-				if(v[2] == "1000k") br = B1000k;
-				if(v[2] == "2000k") br = B2000k;
-				if(v[2] == "4000k") br = B4000k;
+		if(has_colon) {
+			try {
+				DeviceHandle dh = OpenNetworkDevice(uri);
+				return EventStreamHandle(new DeviceEventStream(dh));
 			}
-			DeviceHandle dh = OpenSerialDevice(br, uri);
-			return EventStreamHandle(new DeviceEventStream(dh));
+			catch(const NetSocket::Exception& e) {
+				std::cerr << "URI is not a network address: " << e.what() << std::endl;
+				throw;
+			}
 		}
-		catch(...) {};
+		// try serial
+		if(has_question && has_equal) {
+			try {
+				std::vector<std::string> v;
+				boost::split(v, uri, boost::is_any_of("?="));
+				Baudrate br = B4000k;
+				if(v.size() == 3 && v[1] == "baudrate") {
+					if(v[2] == "921k") br = B921k;
+					if(v[2] == "1000k") br = B1000k;
+					if(v[2] == "2000k") br = B2000k;
+					if(v[2] == "4000k") br = B4000k;
+				}
+				DeviceHandle dh = OpenSerialDevice(br, uri);
+				return EventStreamHandle(new DeviceEventStream(dh));
+			}
+			catch(const SerialPort::IOException& e) {
+				std::cerr << "URI is not a serial port: " << e.what() << std::endl;
+				throw;
+			}
+		}
 		// last try: filesystem
 		try {
 			return OpenFileStream(uri);
 		}
 		catch(...) {
-			std::cerr << "Wrong URI!" << std::endl;
-			throw 0;
+			std::cerr << "URI is not a file!" << std::endl;
+			throw;
 		}
 	}
 
