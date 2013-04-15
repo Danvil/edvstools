a31b79046b7789d64fe738cb65885f2a6830a774
Nicolai Waniek <nicolai.waniek@sphere71.com>
2013-04-15 14:48:51 +0200

    sigaction

--- Examples/example_capture.c
+++ Examples/example_capture.c
@@ -1,25 +1,42 @@
 #include "edvs.h"
+#define __USE_POSIX
 #include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
+
+edvs_stream_handle s;
+edvs_event_t* events = NULL;
+
+void foo(int signum) {
+	fprintf(stderr, "zonk\n");
+	edvs_close(s);
+	free(events);
+}
 
 int main(int argc, char** argv)
 {
+	struct sigaction siginthandler;
+
+	siginthandler.sa_handler = foo;
+	sigemptyset(&siginthandler.sa_mask);
+	siginthandler.sa_flags = 0;
+
+	sigaction(SIGINT, &siginthandler, NULL);
+
+
 	if(argc != 2) {
 		printf("Wrong usage\n");
 		return EXIT_FAILURE;
 	}
-	// run event capture
-	edvs_stream_handle s = edvs_open(argv[1]);
+	s = edvs_open(argv[1]);
+// run event capture
 	size_t num_max_events = 1024;
-	edvs_event_t* events = (edvs_event_t*)malloc(num_max_events*sizeof(edvs_event_t));
+	events = (edvs_event_t*)malloc(num_max_events*sizeof(edvs_event_t));
 	while(!edvs_eos(s)) {
 		ssize_t m = edvs_read(s, events, num_max_events);
 		if(m > 0) {
 			printf("Time %lu: %zd events\n", events->t, m);
 		}
 	}
-	// cleanup
-	edvs_close(s);
-	free(events);
 	return EXIT_SUCCESS;
 }
