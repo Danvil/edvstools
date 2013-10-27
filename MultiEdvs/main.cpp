0f1c30086336796efb6971200de561256a67ae32
David Weikersdorfer <davidw@danvil.de>
2013-10-27 11:33:57 +0100

    added default values for uri1/uri2

--- MultiEdvs/main.cpp
+++ MultiEdvs/main.cpp
@@ -6,16 +6,16 @@
 
 int main(int argc, char *argv[])
 {
-	std::string p_uri1 = "";
-	std::string p_uri2 = "";
+	std::string p_uri1 = "/dev/ttyUSB0?baudrate=4000000";
+	std::string p_uri2 = "/dev/ttyUSB2?baudrate=4000000";
 
 	namespace po = boost::program_options;
 	// Declare the supported options.
 	po::options_description desc("Allowed options");
 	desc.add_options()
 		("help", "produce help message")
-		("uri1", po::value(&p_uri1), "URI to event source (use help for more info)")
-		("uri2", po::value(&p_uri2), "URI to event source (use help for more info)")
+		("uri1", po::value(&p_uri1)->default_value(p_uri1), "URI to event source 1")
+		("uri2", po::value(&p_uri2)->default_value(p_uri2), "URI to event source 2")
 	;
 
 	po::variables_map vm;
