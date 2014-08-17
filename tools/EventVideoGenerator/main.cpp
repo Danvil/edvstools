acc265ecb8a4c0bd7a23b829d6bad404d804fc56
danvil <davidw@danvil.de>
2014-08-17 17:34:22 +0200

    bugfix with default (nocolor) mode

--- tools/EventVideoGenerator/main.cpp
+++ tools/EventVideoGenerator/main.cpp
@@ -266,7 +266,7 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	if(vm.count("colored")) {
+	if(p_colored) {
 		// read events
 		auto events = LoadColoredEvents(p_fn);
 		std::cout << "Read " << events.size() << " events" << std::endl;
